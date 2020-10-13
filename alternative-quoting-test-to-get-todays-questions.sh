bbe01e5359d28f42cd085548f78f8e8150e00c8e
Roland Tanglao <roland@rolandtanglao.com`>
2020-10-13 10:38:41 -0700

    make test script identical to github action version

--- alternative-quoting-test-to-get-todays-questions.sh
+++ alternative-quoting-test-to-get-todays-questions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 TZ=":US/Pacific" timestamp=$(date -u)
-year=$(date -d "${timestamp}" "+%Y")
-month=$(date -d "${timestamp}" "+%m")
-day=$(date -d "${timestamp}" "+%e")
+year=$(date -d "${timestamp}" +%Y)
+month=$(date -d "${timestamp}" +%m)
+day=$(date -d "${timestamp}" +%e)
 echo "./get-creator-answers-questions-for-arbitrary-time-period.rb $year $month $day $year $month $day"
