5cf308ba25d82ea05adb84a1bdb9995d694c25bc
Roland Tanglao <roland@rolandtanglao.com>
2024-06-11 10:32:03 -0700

    add ruby pretty printed version to make linking easier

---
+++ testAPI-issue-3686-rb--one-AAQ-question-pretty-print-of-AAQ-1question-json.rb
@@ -0,0 +1,104 @@
+{
+  "answers" => [
+[ 0] 1222018,
+[ 1] 1222044,
+[ 2] 1222056,
+[ 3] 1222067,
+[ 4] 1222070,
+[ 5] 1222071,
+[ 6] 1222125,
+[ 7] 1222128,
+[ 8] 1222302,
+[ 9] 1222414,
+[10] 1222423,
+[11] 1225628
+],
+  "content" => "<p>Morning,\n</p><p>I've recently switched back to Firefox (for Mac) after being away from Ff for a long time, and have been tinkering with my settings and whatnot to try and get things the way I would like them. I suspect, like most things, that I'm just missing something.&nbsp;;-)\n</p><p>Basically, I don't want Ff to remember any autofill//form information, and I've got that working, but I would like it to be able to respect when a website allows you to click a radio button for \"Remember &lt;Me//This//Etc&gt;\". It does not seem to be doing that at the moment.\n</p><p>Does Ff have a setting that will let websites that you specifically click \"Remember Me\" be remembered, or is it an all or nothing?\n</p><p>Thanks!\n</p>",
+  "created" => "2019-05-13T00:53:40Z",
+  "creator" => {
+"username" => "mattagc",
+"display_name" => "Matt Campbell",
+"avatar" => "https://firefoxusercontent.com/a6acb0932f1dcc47ef32212bc9ad9394"
+},
+       "id" => 1259114,
+ "involved" => [
+[0] {
+  "username" => "jscher2000",
+"display_name" => "jscher2000 - Support Volunteer",
+    "avatar" => "https://firefoxusercontent.com/4c7c89d3462af52bac94074246fd5e98"
+},
+[1] {
+  "username" => "mattagc",
+"display_name" => "Matt Campbell",
+    "avatar" => "https://firefoxusercontent.com/a6acb0932f1dcc47ef32212bc9ad9394"
+},
+[2] {
+  "username" => "cor-el",
+"display_name" => "",
+    "avatar" => "https://firefoxusercontent.com/f369028d14003acbf4f1a9ed0debb2c8"
+}
+],
+"is_archived" => true,
+"is_locked" => false,
+"is_solved" => false,
+  "is_spam" => false,
+ "is_taken" => false,
+"last_answer" => 1225628,
+   "locale" => "en-US",
+ "metadata" => [
+[0] {
+"name" => "useragent",
+"value" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0"
+},
+[1] {
+"name" => "ff_version",
+"value" => "66.0"
+},
+[2] {
+"name" => "os",
+"value" => "Mac OS"
+},
+[3] {
+"name" => "plugins",
+"value" => "* Shockwave Flash 32.0 r0"
+},
+[4] {
+"name" => "product",
+"value" => "desktop"
+},
+[5] {
+"name" => "category",
+"value" => "privacy-and-security"
+}
+],
+     "tags" => [
+[0] {
+"name" => "Mac OS",
+"slug" => "mac-os"
+},
+[1] {
+"name" => "desktop",
+"slug" => "desktop"
+},
+[2] {
+"name" => "privacy-and-security",
+"slug" => "privacy-and-security_1"
+},
+[3] {
+"name" => "Firefox 66.0",
+"slug" => "firefox-660"
+}
+],
+"num_answers" => 12,
+"num_votes_past_week" => 0,
+"num_votes" => 1,
+  "product" => "firefox",
+ "solution" => nil,
+"solved_by" => nil,
+"taken_until" => nil,
+ "taken_by" => nil,
+    "title" => "Firefox doesn't seem to be allowing websites to \"remember <random thing>\".",
+    "topic" => "privacy-and-security",
+"updated_by" => nil,
+  "updated" => "2019-05-27T12:18:25Z"
+}
